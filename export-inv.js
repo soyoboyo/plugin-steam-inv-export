28eb7c2ec44f72b207775e14c270fe26abb6b730
Jacek Zalewski <jacekzalewski95@gmail.com>
2020-12-18 22:37:51 +0100

    Manage edge cases for different item types

--- export-inv.js
+++ export-inv.js
@@ -1,40 +1,96 @@
 document.body.style.border = "10px solid red";
 
-
-
-
+console.log("hello")
 const button = document.createElement("button");
 const controlsContainer = document.createElement("div");
 controlsContainer.style.padding = "10px";
-button.innerHTML = "START SCAN";
-button.addEventListener("click", function () {
-    getItems();
-});
+button.innerHTML = "DOWNLOAD ITEMS";
 controlsContainer.appendChild(button);
+button.addEventListener("click", function () {
+    console.log("button click");
+    let response = getItems();
+    let assets = response.assets;
+    let descriptions = response.descriptions;
+    let itemsMap = new Map();
 
-setTimeout(function () {
-    let buttonPlacement = document.getElementById("active_inventory_page");
-    let parent = buttonPlacement.parentElement;
-    parent.insertBefore(button, buttonPlacement);
-}, 2000);
+    putDescriptionsInMap(descriptions, itemsMap);
+    // assets.reverse();
+    processItems(assets, itemsMap);
+});
+
+let buttonPlacement = document.getElementById("active_inventory_page");
+let parent = buttonPlacement.parentElement;
+parent.insertBefore(button, buttonPlacement);
+// setTimeout(function () {
+//
+// }, 2000);
 
 
-
-pagesLimit = 2;
-const itemsLimit = 50;
-let list = [];
-let csvContent = "data:text/csv;charset=utf-8\n";
+/////////////////////////////////////////////////////////
 
 
-function getItems(){
+function getItems() {
+    console.log("get items");
     let xmlHttp = new XMLHttpRequest();
-    let inventoryURL = "https://steamcommunity.com/inventory/76561198017460423/730/2?l=english&count=75";
-    xmlHttp.open( "GET", inventoryURL, false ); // false for synchronous request
-    xmlHttp.send( null );
-    let items = JSON.parse(xmlHttp.responseText);
-    console.log(items);
-    return xmlHttp.responseText;
+    let inventoryURL = "https://steamcommunity.com/inventory/76561198017460423/730/2?l=english&count=500";
+    xmlHttp.open("GET", inventoryURL, false); // false for synchronous request
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.responseText);
 }
 
+function putDescriptionsInMap(descriptions, itemsMap) {
+    descriptions.forEach((it) => {
+        let uniqueDescription = it.classid + "_" + it.instanceid;
+        itemsMap.set(uniqueDescription, it);
+    })
+}
 
+function processItems(assets, itemsMap) {
+    console.log("process items");
+    let file = "";
+    assets.forEach((a) => {
+        let uniqueAsset = a.classid + "_" + a.instanceid;
+        let item = itemsMap.get(uniqueAsset);
+        file += createLine(item.tags, item.name)
+    })
+    console.log(file);
+    // fs.writeFileSync('items.csv', file);
+}
+
+function createLine(tags, name) {
+    // console.log("create line " + name);
+    let type = tags.find(tag => tag.category === 'Type').localized_tag_name;
+    if (type === 'Container') {
+        return "--------------- container\n";
+    } else if (type === 'Knife') {
+        return "--------------- knife\n";
+    } else if (type === 'Agent') {
+        return "--------------- knife\n";
+    } else if (type === 'Tool') {
+        return "--------------- tool\n";
+    } else if (type === 'Collectible') {
+        return "--------------- tool\n";
+    } else if (type === 'Gloves') {
+        return "--------------- tool\n";
+    } else if (type === 'Music Kit') {
+        return "--------------- music kit\n";
+    } else if (type === 'Patch') {
+        return "--------------- patch\n";
+    }else if (type === 'Pass') {
+        return "--------------- pass\n";
+    }
+
+    let weapon = tags.find(tag => tag.category === 'Weapon')
+    if (weapon !== undefined) {
+        let itemName = name.split("| ")[1];
+        let weapon = tags.find(tag => tag.category === 'Weapon').localized_tag_name;
+        let origin = tags.find(tag => tag.category === 'ItemSet').localized_tag_name;
+        let condition = tags.find(tag => tag.category === 'Exterior').localized_tag_name;
+        let stattrak = tags.find(tag => tag.category === 'Quality').localized_tag_name === "Normal" ? 0 : 1;
+        let rarity = tags.find(tag => tag.category === 'Rarity').localized_tag_name;
+        return itemName + "," + condition + "," + stattrak + "," + weapon + "," + rarity + "," + origin + "\n";
+    } else {
+        return "item not managed ----------------------------" + name + "\n";
+    }
+}
 
--- process_steam_inv.js
+++ process_steam_inv.js
@@ -7,60 +7,97 @@ let assets = fullItems.assets;
 let items = fullItems.descriptions;
 
 let itemsMap = new Map()
-let assetsSet = new Set()
-let descriptionSet = new Set()
 
-function findMissingItems() {
-    items.forEach((it) => {
+document.body.style.border = "10px solid red";
+
+console.log("hello")
+const button = document.createElement("button");
+const controlsContainer = document.createElement("div");
+controlsContainer.style.padding = "10px";
+button.innerHTML = "DOWNLOAD ITEMS";
+controlsContainer.appendChild(button);
+button.addEventListener("click", function () {
+    console.log("button click");
+    let response = getItems();
+    let assets = response.assets;
+    let descriptions = response.descriptions;
+    let itemsMap = new Map();
+
+    putDescriptionsInMap(descriptions, itemsMap);
+    // assets.reverse();
+    processItems(assets, itemsMap);
+});
+
+
+setTimeout(function () {
+    let buttonPlacement = document.getElementById("active_inventory_page");
+    let parent = buttonPlacement.parentElement;
+    parent.insertBefore(button, buttonPlacement);
+}, 2000);
+
+
+/////////////////////////////////////////////////////////
+
+
+function getItems() {
+    console.log("get items");
+    let xmlHttp = new XMLHttpRequest();
+    let inventoryURL = "https://steamcommunity.com/inventory/76561198017460423/730/2?l=english&count=75";
+    xmlHttp.open("GET", inventoryURL, false); // false for synchronous request
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.responseText);
+}
+
+function putDescriptionsInMap(descriptions, itemsMap) {
+    descriptions.forEach((it) => {
         let uniqueDescription = it.classid + "_" + it.instanceid;
         itemsMap.set(uniqueDescription, it);
     })
-    assets.reverse()
 }
 
-function loadItems() {
-    let resultItems = [];
-    items.forEach((item) => {
-        for (const key in item) {
-            const result = Object.fromEntries(
-                Object.entries(item)
-                    .filter(([key]) => key == "name" || key == "tags")
-            )
-            resultItems.push(result);
-        }
-    })
-
-}
-
-function processItems() {
+function processItems(assets, itemsMap) {
+    console.log("process items");
     let file = "";
     assets.forEach((a) => {
         let uniqueAsset = a.classid + "_" + a.instanceid;
         let item = itemsMap.get(uniqueAsset);
         file += createLine(item.tags, item.name)
     })
-    fs.writeFileSync('items.csv', file);
+    console.log(file);
+    // fs.writeFileSync('items.csv', file);
 }
 
 function createLine(tags, name) {
-    let itemName = name.split("| ")[1];
+    console.log("create line " + name);
     if (tags.length === 6) {
-        let stattrak = tags[3].localized_tag_name === "Normal" ? 0 : 1;
-        let weapon = tags[1].localized_tag_name;
-        let origin = tags[2].localized_tag_name;
-        let condition = tags[5].localized_tag_name;
-        let rarity = tags[4].localized_tag_name;
+        let itemName = name.split("| ")[1];
+        let weapon = tags.find(tag => tag.category === 'Weapon').localized_tag_name;
+        console.log("1");
+        let origin = tags.find(tag => tag.category === 'ItemSet').localized_tag_name;
+        console.log("2");
+        let condition = tags.find(tag => tag.category === 'Exterior').localized_tag_name;
+        console.log("3");
+        let stattrak = tags.find(tag => tag.category === 'Quality').localized_tag_name === "Normal" ? 0 : 1;
+        console.log("4");
+        let rarity = tags.find(tag => tag.category === 'Rarity').localized_tag_name;
         return itemName + "," + condition + "," + stattrak + "," + weapon + "," + rarity + "," + origin + "\n";
-    } else if (tags.length === 4) {
-        let stattrak = tags[3].localized_tag_name === "Normal" ? 0 : 1;
-        let weapon = tags[1].localized_tag_name;
-        let origin = tags[2].localized_tag_name;
-        return itemName + "," + "," + stattrak + "," + weapon + "," + origin + "\n";
     } else {
-        return itemName + "\n";
+        return " "
     }
+    // if (tags.length === 6) {
+    //     let stattrak = tags[3].localized_tag_name === "Normal" ? 0 : 1;
+    //     let weapon = tags[1].localized_tag_name;
+    //     let origin = tags[2].localized_tag_name;
+    //     let condition = tags[5].localized_tag_name;
+    //     let rarity = tags[4].localized_tag_name;
+    //     return itemName + "," + condition + "," + stattrak + "," + weapon + "," + rarity + "," + origin + "\n";
+    // } else if (tags.length === 4) {
+    //     let stattrak = tags[3].localized_tag_name === "Normal" ? 0 : 1;
+    //     let weapon = tags[1].localized_tag_name;
+    //     let origin = tags[2].localized_tag_name;
+    //     return itemName + "," + "," + stattrak + "," + weapon + "," + origin + "\n";
+    // } else {
+    //     return itemName + "\n";
+    // }
 }
 
-findMissingItems()
-loadItems()
-processItems()
